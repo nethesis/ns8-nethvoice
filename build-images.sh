5bca280703d11ea56cb198fc6327f70cab6a3bd9
Stefano Fancello <stefano.fancello@nethesis.it>
2022-02-14 16:21:14 +0100

    Use mariadb instead of echoserver

--- build-images.sh
+++ build-images.sh
@@ -30,7 +30,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@any:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/jmalloc/echo-server:latest" \
+    --label="org.nethserver.images=docker.io/library/mariadb:latest" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
