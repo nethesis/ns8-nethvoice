f39084d44289dabd9cbac85054539ed5985093bc
Stefano Fancello <stefano.fancello@nethesis.it>
2022-02-17 16:00:07 +0100

    Add Apache + PHP5.6

--- build-images.sh
+++ build-images.sh
@@ -31,7 +31,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@any:routeadm" \
     --label="org.nethserver.tcp-ports-demand=3" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=docker.io/library/mariadb:latest" \
+    --label="org.nethserver.images=docker.io/library/mariadb:latest docker.io/library/php:5.6-apache" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
