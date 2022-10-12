9f27d2f12bf352eb4a04ebffa7af3ea74e16493c
Stefano Fancello <stefano.fancello@nethesis.it>
2022-10-12 16:09:38 +0200

    Remove obsolete files

--- imageroot/bin/cleanuptcmaint.php
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env php
-<?php
-
-//include bootstrap
-$restrict_mods = array('timeconditions' => true);
-$bootstrap_settings['freepbx_auth'] = false;
-include '/etc/freepbx.conf';
-$tc = \FreePBX::Timeconditions();
-$tc->cleanuptcmaint();
