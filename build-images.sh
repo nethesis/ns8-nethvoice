907f3ddc899f6083e33664f508890ee59127e7d3
Stefano Fancello <stefano.fancello@nethesis.it>
2022-10-20 18:11:45 +0200

    containers build instruction added to build-image.sh script

--- build-images.sh
+++ build-images.sh
@@ -36,58 +36,200 @@ buildah config \
 
 images+=("${repobase}/${reponame}"):
 
-reponame="freepbx"
+
+
+
+########################
+##      Asterisk      ##
+########################
+echo "[*] Build Asterisk container"
+reponame="asterisk"
+container=$(buildah from centos:7)
+buildah add "${container}" imageroot/asterisk/root/ /
+buildah run "${container}" yum -y install asterisk18-core asterisk18-addons-core asterisk18-dahdi asterisk18-odbc asterisk18-voicemail asterisk18-voicemail-odbcstorage unixODBC
+buildah run "${container}" rm -fr /var/cache/yum
+buildah config --entrypoint='["/entrypoint.sh"]' "${container}"
+
+# Commit the image
+buildah commit "${container}" "${repobase}/${reponame}"
+# Append the image URL to the images array
+images+=("${repobase}/${reponame}"):
+
+
+
+
+##########################
+##      FreePBX 14      ##
+##########################
+echo "[*] Build FreePBX container"
+reponame="freepbx14"
+
 container=$(buildah from docker.io/library/php:5.6-apache)
+buildah add "${container}" imageroot/freepbx/root/ /
+buildah run "${container}" groupadd -r asterisk
+buildah run "${container}" useradd -r -s /bin/false -d /var/lib/asterisk -M -c 'Asterisk User' -g asterisk asterisk
 
-# Copy entrypoint script into freepbx container
-buildah add "${container}" imageroot/freepbx/entrypoint.sh /entrypoint.sh
-
-# Copy mysql initializzation data
-buildah add "${container}" imageroot/freepbx/initdb.d /
+buildah run "${container}" /bin/sh <<EOF
+sed -i 's/<VirtualHost \*:80>/<VirtualHost \*:\$\{APACHE_PORT\}>/' /etc/apache2/sites-enabled/000-default.conf
+sed -i 's/Listen 80/Listen \$\{APACHE_PORT\}/' /etc/apache2/ports.conf
+sed -i 's/Listen 443/Listen \$\{APACHE_SSL_PORT\}/' /etc/apache2/ports.conf
+echo '\n: \${APACHE_PORT:=80}\nexport APACHE_PORT\n: \${APACHE_SSL_PORT:=443}\nexport APACHE_SSL_PORT\n' >> /etc/apache2/envvars
+EOF
 
 buildah config \
     --entrypoint='["/entrypoint.sh"]' \
     "${container}"
 
+# Install required packages
+buildah run "${container}" apt-get update
+buildah run "${container}" apt install -y gnupg mycli libldap2-dev
+buildah run "${container}" apt install -y cron # TODO needed by freepbx cron module. To remove.
+
+# install PHP additional modules
+buildah run "${container}" docker-php-source extract
+
+# install pdo_mysql
+buildah run "${container}" docker-php-ext-configure pdo_mysql
+buildah run "${container}" docker-php-ext-install pdo_mysql
+
+# install php gettext
+buildah run "${container}" docker-php-ext-configure gettext
+buildah run "${container}" docker-php-ext-install gettext
+
+# install ldap
+buildah run "${container}" ln -s /usr/lib/x86_64-linux-gnu/libldap.so /usr/lib/libldap.so
+buildah run "${container}" docker-php-ext-configure ldap
+buildah run "${container}" docker-php-ext-install ldap
+
+# install php semaphores (sysvsem)
+buildah run "${container}" docker-php-ext-configure sysvsem
+buildah run "${container}" docker-php-ext-install sysvsem
+
+# TODO install pdo_odbc
+#buildah run "${container}" apt-get update
+#buildah run "${container}" apt install -y unixodbc unixodbc-dev
+#buildah run "${container}" docker-php-ext-configure pdo_odbc --with-pdo-odbc=unixODBC
+#buildah run "${container}" docker-php-ext-install pdo_odbc
+
+# Use PHP development ini configuration and enable logging on syslog
+buildah run "${container}" cp -a "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+buildah run "${container}" sed -i 's/^;error_log = syslog/error_log = \/dev\/stderr/' $PHP_INI_DIR/php.ini
+echo "error_log = /dev/stderr" | buildah run "${container}" tee -a "$PHP_INI_DIR/conf.d/freepbx.ini"
+echo "variables_order = "EGPCS"" | buildah run "${container}" tee -a "$PHP_INI_DIR/conf.d/freepbx.ini"
+
+# Enable environment variables
+buildah run "${container}" sed -i 's/^variables_order = "GPCS"/variables_order = "EGPCS"/' $PHP_INI_DIR/php.ini
+
+# enable apache rewrite module
+buildah run "${container}" a2enmod rewrite proxy*
+
+# remove php sources
+buildah run "${container}" docker-php-source delete
+
+# TODO REMOVE BEFORE DEPLOY
+buildah run "${container}" apt-get install -y vim telnet
+
+# clean apt cache
+buildah run "${container}" apt-get clean autoclean
+buildah run "${container}" apt-get autoremove --yes
+buildah run "${container}" rm -rf /var/lib/dpkg/info/* /var/lib/cache/* /var/lib/log/*
+buildah run "${container}" touch /var/lib/dpkg/status
+
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
-
 # Append the image URL to the images array
 images+=("${repobase}/${reponame}"):
 
-# Build Asterisk container
 
-reponame="asterisk"
-container=$(buildah from centos:7)
 
-buildah run $container -- bash -s <<'EOF'
-yum install -y http://mirror.nethserver.org/nethserver/nethserver-release-7.rpm
-yum install -y epel-release
-yum install -y \
-    asterisk13-core \
-    asterisk13-odbc \
-    asterisk13-addons-mysql \
-    asterisk13-resample \
-    asterisk-codecs \
-    asterisk13-voicemail-odbcstorage \
-    asterisk13-dahdi \
-    asterisk13-speex \
-    asterisk13-addons-core \
-    asterisk-sounds-extra-en-ulaw \
-    unixODBC \
-    mysql-connector-odbc
-yum clean all
-rm -rf /var/cache/yum
+
+########################
+##      Tancredi      ##
+########################
+echo "[*] Build Tancredi container"
+reponame="tancredi"
+container=$(buildah from docker.io/library/php:7-apache)
+buildah config --entrypoint='["/entrypoint.sh"]' "${container}"
+buildah add "${container}"  imageroot/tancredi/root/ /
+buildah run "${container}" /bin/sh <<'EOF'
+apt update
+apt install -y libapache2-mod-xsendfile zip
+ln -sf /etc/apache2/sites-available/tancredi.conf /etc/apache2/sites-enabled/tancredi.conf
+
+sed -i 's/<VirtualHost \*:80>/<VirtualHost \*:$\{TANCREDIPORT\}>/' /etc/apache2/sites-enabled/000-default.conf
+sed -i 's/Listen 80/Listen $\{TANCREDIPORT\}/' /etc/apache2/ports.conf
+sed -i 's/Listen 443/Listen $\{TANCREDI_SSL_PORT\}/' /etc/apache2/ports.conf
+echo -e '\n: ${TANCREDIPORT:=80}\nexport TANCREDIPORT\n: ${TANCREDI_SSL_PORT:=443}\nexport TANCREDI_SSL_PORT\n' | buildah run "${container}" tee -a /etc/apache2/envvars
+
+# Install Tancredi files
+mkdir /usr/share/tancredi/
+curl -L https://github.com/nethesis/tancredi/archive/refs/heads/master.tar.gz -o - | tar xzp --strip-component=1 -C /usr/share/tancredi/ tancredi-master/data/ tancredi-master/public/ tancredi-master/scripts/ tancredi-master/src/ tancredi-master/composer.json tancredi-master/composer.lock
+
+BRANCH=otherdb
+curl -L https://github.com/nethesis/nethserver-tancredi/archive/refs/heads/${BRANCH}.tar.gz -o - | tar xzp --strip-component=2 -C / nethserver-tancredi-${BRANCH}/root/usr/share/tancredi/ nethserver-tancredi-${BRANCH}/root/var/lib/tancredi
+cd /usr/share/tancredi/
+curl -s https://getcomposer.org/installer | php
+COMPOSER_ALLOW_SUPERUSER=1 php composer.phar install --no-dev
+rm -fr /usr/share/tancredi/src/Entity/SampleFilter.php /usr/share/tancredi/composer.phar /usr/share/tancredi/composer.json /usr/share/tancredi/composer.lock
+
+# install pdo_mysql
+docker-php-source extract
+docker-php-ext-configure pdo_mysql
+docker-php-ext-install pdo_mysql
+docker-php-source delete
+
+# clean apt cache
+apt-get clean autoclean
+apt-get autoremove --yes
+rm -rf /var/lib/dpkg/info/* /var/lib/cache/* /var/lib/log/*
+touch /var/lib/dpkg/status
 EOF
 
-buildah config \
-    --workingdir=/var/lib/asterisk \
-    "${container}"
+buildah run "${container}" cp -a "$PHP_INI_DIR/php.ini-production" "$PHP_INI_DIR/php.ini"
+buildah run "${container}" sed -i 's/^;error_log = syslog/error_log = \/dev\/stderr/' $PHP_INI_DIR/php.ini
 
-# Commit the asterisk image
+# Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
+# Append the image URL to the images array
+images+=("${repobase}/${reponame}"):
 
-images+=("${repobase}/${reponame}")
+
+
+
+#######################
+##      NethCTI      ##
+#######################
+echo "[*] Build nethcti container"
+reponame="nethcti"
+container=$(buildah from docker.io/library/node:14)
+buildah add "${container}" imageroot/nethcti/root/ /
+buildah config --workingdir /usr/lib/node/nethcti-server "${container}"
+buildah config --entrypoint '["npm", "start"]' "${container}"
+buildah commit "${container}" nethcti-server
+
+# Commit the image
+buildah commit "${container}" "${repobase}/${reponame}"
+# Append the image URL to the images array
+images+=("${repobase}/${reponame}"):
+
+
+
+
+#############################
+##      Janus Gateway      ##
+#############################
+echo "[*] Build Janus Gateway container"
+reponame="janus"
+container=$(buildah from docker.io/canyan/janus-gateway:master)
+buildah add "${container}" imageroot/janus/root/ /
+buildah config --entrypoint='["/entrypoint.sh"]' "${container}"
+buildah commit "${container}" janus
+
+# Commit the image
+buildah commit "${container}" "${repobase}/${reponame}"
+# Append the image URL to the images array
+images+=("${repobase}/${reponame}"):
+
 
 # Setup CI when pushing to Github. 
 # Warning! docker::// protocol expects lowercase letters (,,)
