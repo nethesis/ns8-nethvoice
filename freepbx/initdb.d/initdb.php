84d4e897ced8f60b477eaf84af619686d5a8275c
Stefano Fancello <stefano.fancello@nethesis.it>
2023-02-01 17:57:32 +0100

    use $_ENV instead of getenv()

--- freepbx/initdb.d/initdb.php
+++ freepbx/initdb.d/initdb.php
@@ -9,21 +9,21 @@ include_once '/etc/freepbx_db.conf';
 
 // update freepbx settings
 $vars = array(
-	'AMPDBUSER' => getenv('AMPDBUSER'),
-	'AMPDBPASS' => getenv('AMPDBPASS'),
-	'ASTMANAGERHOST' => (empty(getenv('ASTMANAGERHOST')) ? '127.0.0.1' : getenv('ASTMANAGERHOST')),
-	'ASTMANAGERPORT' => (empty(getenv('ASTMANAGERPORT')) ? '5038' : getenv('ASTMANAGERPORT')),
-	'AMPMGRUSER' => (empty(getenv('AMPMGRUSER')) ? 'admin' : getenv('AMPMGRUSER')),
-	'AMPMGRPASS' => (empty(getenv('AMPMGRPASS')) ? 'amp111' : getenv('AMPMGRPASS')),
+	'AMPDBUSER' => $_ENV['AMPDBUSER'],
+	'AMPDBPASS' => $_ENV['AMPDBPASS'],
+	'ASTMANAGERHOST' => (empty($_ENV['ASTMANAGERHOST']) ? '127.0.0.1' : $_ENV['ASTMANAGERHOST']),
+	'ASTMANAGERPORT' => (empty($_ENV['ASTMANAGERPORT']) ? '5038' : $_ENV['ASTMANAGERPORT']),
+	'AMPMGRUSER' => (empty($_ENV['AMPMGRUSER']) ? 'admin' : $_ENV['AMPMGRUSER']),
+	'AMPMGRPASS' => (empty($_ENV['AMPMGRPASS']) ? 'amp111' : $_ENV['AMPMGRPASS']),
 	'CDRDBHOST' => '127.0.0.1',
-	'CDRDBPORT' => getenv('NETHVOICE_MARIADB_PORT'),
+	'CDRDBPORT' => $_ENV['NETHVOICE_MARIADB_PORT'],
 	'CDRDBNAME' => 'asteriskcdrdb',
-	'CDRDBUSER' => getenv('CDRDBUSER'),
-	'CDRDBPASS' => getenv('CDRDBPASS'),
-	'AMPASTERISKGROUP' => (empty(getenv('AMPASTERISKGROUP')) ? 'asterisk' : getenv('AMPASTERISKGROUP')),
-	'AMPASTERISKUSER' => (empty(getenv('AMPASTERISKUSER')) ? 'asterisk' : getenv('AMPASTERISKUSER')),
-	'AMPASTERISKWEBGROUP' => (empty(getenv('AMPASTERISKWEBGROUP')) ? 'asterisk' : getenv('AMPASTERISKWEBGROUP')),
-	'AMPASTERISKWEBUSER' => (empty(getenv('AMPASTERISKWEBUSER')) ? 'asterisk' : getenv('AMPASTERISKWEBUSER')),
+	'CDRDBUSER' => $_ENV['CDRDBUSER'],
+	'CDRDBPASS' => $_ENV['CDRDBPASS'],
+	'AMPASTERISKGROUP' => (empty($_ENV['AMPASTERISKGROUP']) ? 'asterisk' : $_ENV['AMPASTERISKGROUP']),
+	'AMPASTERISKUSER' => (empty($_ENV['AMPASTERISKUSER']) ? 'asterisk' : $_ENV['AMPASTERISKUSER']),
+	'AMPASTERISKWEBGROUP' => (empty($_ENV['AMPASTERISKWEBGROUP']) ? 'asterisk' : $_ENV['AMPASTERISKWEBGROUP']),
+	'AMPASTERISKWEBUSER' => (empty($_ENV['AMPASTERISKWEBUSER']) ? 'asterisk' : $_ENV['AMPASTERISKWEBUSER']),
 );
 
 $exec = [];
@@ -57,7 +57,7 @@ if (empty($res)) {
         // prxycti user doesn't exists and needs to be created
         $sql = "INSERT INTO `arimanager` (`name`, `password`, `password_format`, `read_only`) VALUES ('proxycti',?,'plain',1)";
         $stmt = $db->prepare($sql);
-        $stmt->execute([getenv('NETHCTI_AMI_PASSWORD')]);
+        $stmt->execute([$_ENV['NETHCTI_AMI_PASSWORD']]);
 
         // Get proxycti entry id
         $id = $db->lastInsertId();
@@ -65,18 +65,18 @@ if (empty($res)) {
         // write manager entry
         $sql = "INSERT INTO `manager` (`manager_id`, `name`, `secret`, `deny`, `permit`, `read`, `write`, `writetimeout`) VALUES (?,'proxycti',?,'0.0.0.0/0.0.0.0','127.0.0.1/255.255.255.0','system,call,log,verbose,command,agent,user,config,dtmf,reporting,cdr,dialplan,originate','system,call,log,verbose,command,agent,user,config,dtmf,reporting,cdr,dialplan,originate',100);";
         $stmt = $db->prepare($sql);
-        $stmt->execute([$id,getenv('NETHCTI_AMI_PASSWORD')]);
+        $stmt->execute([$id,$_ENV['NETHCTI_AMI_PASSWORD']]);
 
         // Enable needreload
         $db->query("UPDATE admin SET value = 'true' WHERE variable = 'need_reload'");
-} else if ($res[0][0] !== getenv('NETHCTI_AMI_PASSWORD')) {
+} else if ($res[0][0] !== $_ENV['NETHCTI_AMI_PASSWORD']) {
 	// user already exists, but password is different
         $sql = "UPDATE `arimanager` SET `password` = ? WHERE `name`='proxycti'";
         $stmt = $db->prepare($sql);
-        $stmt->execute([getenv('NETHCTI_AMI_PASSWORD')]);
+        $stmt->execute([$_ENV['NETHCTI_AMI_PASSWORD']]);
         $sql = "UPDATE `manager` SET `secret` = ? WHERE `name`='proxycti'";
         $stmt = $db->prepare($sql);
-        $stmt->execute([getenv('NETHCTI_AMI_PASSWORD')]);
+        $stmt->execute([$_ENV['NETHCTI_AMI_PASSWORD']]);
 
         // Enable needreload
         $db->query("UPDATE admin SET value = 'true' WHERE variable = 'need_reload'");
