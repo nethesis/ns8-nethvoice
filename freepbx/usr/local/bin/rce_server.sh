32d14b7b832c33a3a913a455ae63df1ef4a251e5
Stefano Fancello <stefano.fancello@nethesis.it>
2024-01-24 18:17:14 +0100

    Use asterisk as user for rce

--- freepbx/usr/local/bin/rce_server.sh
+++ freepbx/usr/local/bin/rce_server.sh
@@ -7,4 +7,4 @@ trap "rm -f $COMMAND_SOCKET_PATH" EXIT
 
 # launch a worker for each command received from the socket.
 # the worker will send back the IO socket path to the client.
-socat -d -d UNIX-LISTEN:"$COMMAND_SOCKET_PATH",fork SYSTEM:"/usr/local/bin/rce_worker.sh",chroot=/var/lib/asterisk,su-d=asterisk
+socat -d -d UNIX-LISTEN:"$COMMAND_SOCKET_PATH",fork,user=asterisk,group=asterisk SYSTEM:"/usr/local/bin/rce_worker.sh"
--- freepbx/usr/local/bin/rce_worker.sh
+++ freepbx/usr/local/bin/rce_worker.sh
@@ -7,7 +7,7 @@ read COMMAND
 IO_SOCKET_PATH="/run/rce/io_$$.sock"
 
 # launch command received from arguments with io on the socket
-socat -d -d UNIX-LISTEN:"$IO_SOCKET_PATH" EXEC:"$COMMAND",pty &
+socat -d -d UNIX-LISTEN:"$IO_SOCKET_PATH",user=asterisk,group=asterisk EXEC:"$COMMAND",pty,su-d=asterisk &
 
 # command ready to be launched and waiting for connection. Send the socket path to the client.
 echo $IO_SOCKET_PATH
