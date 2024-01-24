96fef2729fd2e2f7b67d2c9e8a5877a1348d3976
Stefano Fancello <stefano.fancello@nethesis.it>
2024-01-24 12:02:42 +0100

    less bloating on rce_client

--- asterisk/usr/local/bin/rce_client.sh
+++ asterisk/usr/local/bin/rce_client.sh
@@ -2,14 +2,10 @@
 
 SOCKET_PATH="/run/rce/command.sock"
 
-cleanup() {
-    rm -f "$IO_SOCKET_PATH"
-}
-echo "$0 $@"
 # Send the command received as argument to the server
 IO_SOCKET_PATH=$(echo "$0 $@" | socat - UNIX-CONNECT:"$SOCKET_PATH")
 echo "received socket path: ${IO_SOCKET_PATH}"
-trap cleanup EXIT
+trap "rm -f $IO_SOCKET_PATH" EXIT
 
 # connect IO to the new socket
 socat UNIX-CONNECT:"$IO_SOCKET_PATH" STDIO
