56c9f585f388dd9814f34a3df90cab53466ff01a
Stefano Fancello <stefano.fancello@nethesis.it>
2024-01-24 13:01:22 +0100

    fix permissions and ownership

--- freepbx/usr/local/bin/rce_server.sh
+++ freepbx/usr/local/bin/rce_server.sh
@@ -7,4 +7,4 @@ trap "rm -f $COMMAND_SOCKET_PATH" EXIT
 
 # launch a worker for each command received from the socket.
 # the worker will send back the IO socket path to the client.
-socat -d -d UNIX-LISTEN:"$COMMAND_SOCKET_PATH",fork SYSTEM:"/usr/local/bin/rce_worker.sh"
+socat -d -d UNIX-LISTEN:"$COMMAND_SOCKET_PATH",fork SYSTEM:"/usr/local/bin/rce_worker.sh",chroot=/var/lib/asterisk,su-d=asterisk
