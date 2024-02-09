f67dd6aff5537e00b1269180a65e81edf8c2841c
Stefano Fancello <stefano.fancello@nethesis.it>
2024-02-09 12:41:54 +0100

    Change default maximum lookup passwords length

--- mariadb/docker-entrypoint-initdb.d/10_asterisk.outboundlookup-schema.sql
+++ mariadb/docker-entrypoint-initdb.d/10_asterisk.outboundlookup-schema.sql
@@ -8,6 +8,6 @@ CREATE TABLE `outboundlookup` (
   `mysql_dbname` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `mysql_query` varchar(1024) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `mysql_username` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `mysql_password` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `mysql_password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `mysql_charset` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
