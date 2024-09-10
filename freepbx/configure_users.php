5ce992fac6fd917c77609693d87a713d4e0160c6
Stefano Fancello <gentoo.stefano@gmail.com>
2024-09-10 12:31:38 +0200

    Add comments

--- freepbx/configure_users.php
+++ freepbx/configure_users.php
@@ -28,11 +28,12 @@ $stmt->execute();
 $results = $stmt->fetchAll(\PDO::FETCH_ASSOC);
 
 if ($_ENV['NETHVOICE_LDAP_SCHEMA'] === 'ad') {
-	# extract username and domain
+	# check if DOMAIN is in the right format
 	if (!preg_match('/DC=/i', $_ENV['NETHVOICE_LDAP_BASE'])) {
 		echo "Invalid DOMAIN format\n";
 		exit(1);
 	}
+	# extract username from LDAP user
 	if (preg_match('/^([^@]*)@([^@]*)$/', $_ENV['NETHVOICE_LDAP_USER'],$tmp)) {
 		# user: username@domain
 		$username = $tmp[1];
