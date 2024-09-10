c75683b1dbf9d181b573ee61878e65a52c5c3f17
Stefano Fancello <gentoo.stefano@gmail.com>
2024-09-10 12:20:05 +0200

    Case insensitive domain extraction from NETHVOICE_LDAP_BASE

--- freepbx/configure_users.php
+++ freepbx/configure_users.php
@@ -32,10 +32,10 @@ if ($_ENV['NETHVOICE_LDAP_SCHEMA'] === 'ad') {
 	if (preg_match('/^([^@]*)@([^@]*)$/', $_ENV['NETHVOICE_LDAP_USER'],$tmp)) {
 		# user: username@domain
 		$username = $tmp[1];
-	} elseif (preg_match('/^([^\\\\]*)\\\\([^\\\\]*)$/', $_ENV['NETHVOICE_LDAP_USER'],$tmp) && preg_match('/DC=/', $_ENV['NETHVOICE_LDAP_BASE'])) {
+	} elseif (preg_match('/^([^\\\\]*)\\\\([^\\\\]*)$/', $_ENV['NETHVOICE_LDAP_USER'],$tmp) && preg_match('/DC=/i', $_ENV['NETHVOICE_LDAP_BASE'])) {
 		# user: domain\username
 		$username = $tmp[2];
-	} elseif (preg_match('/^([^@]*)$/', $_ENV['NETHVOICE_LDAP_USER'],$tmp) && preg_match('/DC=/', $_ENV['NETHVOICE_LDAP_BASE'])) {
+	} elseif (preg_match('/^([^@]*)$/', $_ENV['NETHVOICE_LDAP_USER'],$tmp) && preg_match('/DC=/i', $_ENV['NETHVOICE_LDAP_BASE'])) {
 		# user: username
 		$username = $tmp[1];
 	} else {
@@ -48,7 +48,7 @@ if ($_ENV['NETHVOICE_LDAP_SCHEMA'] === 'ad') {
 		"dn" => $_ENV['NETHVOICE_LDAP_BASE'],
 		"username" => $username,
 		"password" => $_ENV['NETHVOICE_LDAP_PASS'],
-		"domain" => preg_replace(['/DC=/', '/,/'], ['', '.'],$_ENV['NETHVOICE_LDAP_BASE']),
+		"domain" => preg_replace(['/DC=/', '/,/i'], ['', '.'],$_ENV['NETHVOICE_LDAP_BASE']),
 		"connection" => '',
 		"localgroups" => '0',
 		"createextensions" => '',
