nethesis/ns8-nethvoice

commit 78eb9b9df32e459752375bfcd7eeea6790140dee

clear asterisk network settings on migration and remove wrong default network (#287)

mariadb/docker-entrypoint-initdb.d/50_asterisk.kvstore_Sipsettings.sql
@@ -85,4 +85,3 @@ INSERT INTO `kvstore_Sipsettings` (`key`,`val`,`type`,`id`) VALUES
 ("tlsextip-0.0.0.0","",NULL,"noid"),
 ("tlslocalnet-0.0.0.0","",NULL,"noid"),
 ("pjsip.identifers.order","[\"EI_ip\",\"EI_username\",\"EI_anonymous\",\"EI_header\",\"EI_auth_username\"]",NULL,"noid"),
-("localnets","[{\"net\":\"172.25.4.0\",\"mask\":\"23\"}]","json-arr","noid");

imageroot/actions/import-module/40mysql
@@ -31,6 +31,9 @@ http_sql_hack='INSERT INTO `freepbx_settings` (`keyword`,`value`,`name`,`level`,
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$sip_sql_hack"
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$iax_sql_hack"
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e "$http_sql_hack"
+/usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e 'UPDATE sipsettings SET data="" WHERE keyword="externip_val"'
+/usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e 'UPDATE kvstore_Sipsettings SET val="" WHERE `key`="externip"'
+/usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} asterisk -e 'DELETE FROM kvstore_Sipsettings WHERE `key`="localnets"'
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} nethcti3 -e 'INSERT INTO user_dbconn (`id`,`host`,`port`,`type`,`user`,`pass`,`name`,`creation`) VALUES (1,"127.0.0.1","'${NETHVOICE_MARIADB_PORT}'","mysql","'${NETHCTI_DB_USER}'","'${NETHCTI_DB_PASSWORD}'","phonebook",NOW())'
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} nethcti3 -e 'INSERT INTO user_dbconn (`id`,`host`,`port`,`type`,`user`,`pass`,`name`,`creation`) VALUES (2,"127.0.0.1","'${NETHVOICE_MARIADB_PORT}'","mysql","'${AMPDBUSER}'","'${AMPDBPASS}'","asteriskcdrdb",NOW())'
 /usr/bin/mysql -uroot -p${MARIADB_ROOT_PASSWORD} -e "GRANT ALL on asterisk.* to '${AMPDBUSER}'@'127.0.0.1' identified by '${AMPDBPASS}'"

commit 912ae575f2f2e5999e1df147bccaa06d7ba90158

mariadb: fix missing semicolon
Correctly terminate the SQL query in one of the database initialization
scripts.

Typo introduced by PR:
https://github.com/nethesis/ns8-nethvoice/pull/287.

mariadb/docker-entrypoint-initdb.d/50_asterisk.kvstore_Sipsettings.sql
@@ -84,4 +84,4 @@ INSERT INTO `kvstore_Sipsettings` (`key`,`val`,`type`,`id`) VALUES
 ("tlsdomain-0.0.0.0","",NULL,"noid"),
 ("tlsextip-0.0.0.0","",NULL,"noid"),
 ("tlslocalnet-0.0.0.0","",NULL,"noid"),
-("pjsip.identifers.order","[\"EI_ip\",\"EI_username\",\"EI_anonymous\",\"EI_header\",\"EI_auth_username\"]",NULL,"noid"),
+("pjsip.identifers.order","[\"EI_ip\",\"EI_username\",\"EI_anonymous\",\"EI_header\",\"EI_auth_username\"]",NULL,"noid");