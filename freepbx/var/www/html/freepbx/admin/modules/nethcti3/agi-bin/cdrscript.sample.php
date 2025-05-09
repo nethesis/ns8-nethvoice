7e5ef5e139da93b9b7bf53fd4588a811fe84e488
Stefano Fancello <gentoo.stefano@gmail.com>
2025-05-09 17:24:15 +0200

    feat(custom): launch custom scripts when call arrives and at hangup

---
+++ freepbx/var/www/html/freepbx/admin/modules/nethcti3/agi-bin/cdrscript.sample.php
@@ -0,0 +1,29 @@
+#!/usr/bin/env php
+<?php
+
+/*
+This is a sample script that is executed by Asterisk when NETHCTI_CDR_SCRIPT variable is set to /var/lib/asterisk/agi-bin/cdrscript.sample.php
+
+Arguments:
+1   source
+2   channel
+3   endtime
+4   duration
+5   amaflags
+6   uniqueid
+7   callerid
+8   starttime
+9   answertime
+10  destination
+11  disposition
+12  lastapplication
+13  billableseconds
+14  destinationcontext
+15  destinationchannel
+16  accountcode
+*/
+
+
+//$agi = new AGI();
+
+
---
+++ freepbx/var/www/html/freepbx/admin/modules/nethcti3/agi-bin/cdrscript_in.sample.php
@@ -0,0 +1,27 @@
+#!/usr/bin/env php
+<?php
+
+/*
+This is a sample script that is executed by Asterisk when NETHCTI_CDR_SCRIPT_IN variable is set to /var/lib/asterisk/agi-bin/cdrscript_in.sample.php
+
+Arguments:
+1   source
+2   channel
+3   (empty)
+4   (empty)
+5   amaflags
+6   uniqueid
+7   callerid
+8   starttime
+9   (empty)
+10  (empty)
+11  (empty)
+12  lastapplication
+13  (empty)
+14  destinationcontext
+15  destinationchannel
+16  accountcode
+*/
+
+
+//$agi = new AGI();
