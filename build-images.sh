011b67c3cc12f158086060cf71143e59191f727f
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2025-09-29 12:45:50 +0200

    feat: added MQTT client with websocket broadcast

--- build-images.sh
+++ build-images.sh
@@ -87,7 +87,7 @@ images+=("${repobase}/${reponame}")
 ##    NethCTI Middleware   ##
 #############################
 reponame="nethvoice-cti-middleware"
-container=$(buildah from ghcr.io/nethesis/nethcti-middleware:v0.2.1)
+container=$(buildah from ghcr.io/nethesis/nethcti-middleware:satellite-mqtt)
 
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
