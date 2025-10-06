04ec31f6581508260a7f1ddcb668795427a53c0e
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2025-10-06 09:36:13 +0200

    Update build-images.sh

--- build-images.sh
+++ build-images.sh
@@ -171,7 +171,7 @@ images+=("${repobase}/${reponame}")
 ##########################
 echo "[*] Build Satellite container"
 reponame="nethvoice-satellite"
-container=$(buildah from ghcr.io/nethesis/satellite:Stell0-patch-3)
+container=$(buildah from ghcr.io/nethesis/satellite:0.0.2)
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
 buildah commit "${container}" "${repobase}/${reponame}:${IMAGETAG:-latest}"
