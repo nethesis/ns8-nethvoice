42fb2ab2c977e4aaee7bd36588c7914fc4acf1b8
Edoardo Spadoni <edoardo.spadoni@nethesis.it>
2025-11-12 09:42:32 +0100

    chore(deps): update ghcr.io/nethesis/nethvoice-cti docker tag to v0.13.3
    Reference:
    - https://github.com/NethServer/dev/issues/7734
    - https://github.com/NethServer/dev/issues/7712

--- build-images.sh
+++ build-images.sh
@@ -100,7 +100,7 @@ images+=("${repobase}/${reponame}")
 ##      NethCTI Client     ##
 #############################
 reponame="nethvoice-cti-ui"
-container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.13.2)
+container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.13.3)
 
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
