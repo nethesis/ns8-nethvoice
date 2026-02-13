b05c5bde213afe5753f6e8401955ca026013e3e3
Stefano <gentoo.stefano@gmail.com>
2026-02-13 10:59:46 +0100

    Update Satellite container version to 0.0.9
    Fix memory leak when Deepgram connection fails
    https://github.com/NethServer/dev/issues/7872

--- build-images.sh
+++ build-images.sh
@@ -171,7 +171,7 @@ images+=("${repobase}/${reponame}")
 ##########################
 echo "[*] Build Satellite container"
 reponame="nethvoice-satellite"
-container=$(buildah from ghcr.io/nethesis/satellite:0.0.8)
+container=$(buildah from ghcr.io/nethesis/satellite:0.0.9)
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
 buildah commit "${container}" "${repobase}/${reponame}:${IMAGETAG:-latest}"
