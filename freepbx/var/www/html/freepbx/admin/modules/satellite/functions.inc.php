ad503264e7c7a0880e859203a463d161e54f9373
Stefano Fancello <gentoo.stefano@gmail.com>
2026-03-04 16:41:30 +0100

    fix(STT dialplan): set sanitized variables before calling mixmonitor

--- freepbx/var/www/html/freepbx/admin/modules/satellite/functions.inc.php
+++ freepbx/var/www/html/freepbx/admin/modules/satellite/functions.inc.php
@@ -14,12 +14,12 @@ function satellite_get_config_late($engine) {
         if (!empty($_ENV['SATELLITE_CALL_TRANSCRIPTION_ENABLED']) && $_ENV['SATELLITE_CALL_TRANSCRIPTION_ENABLED'] == 'True') {
             // Add a call Satellite when call is answered in macro-dial-one adding it in D_OPTIONS variable
             $ext->splice('macro-dial-one','s','dial', new \ext_setvar('D_OPTIONS', '${D_OPTIONS}U(satellite^s^1)'),'', -1);
-            // Sanitize variables to prevent shell injection in mixmonitor command
-            $ext->splice('macro-dial-one', 's', 'dial', new \ext_setvar('SAT_SAFE_UID', '${FILTER(A-Za-z0-9._-,${UNIQUEID})}'), '', -1);
-            $ext->splice('macro-dial-one', 's', 'dial', new \ext_setvar('SAT_SAFE_DST', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(dst_cnam)})}'), '', -1);
-            $ext->splice('macro-dial-one', 's', 'dial', new \ext_setvar('SAT_SAFE_SRC', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(cnam)})}'), '', -1);
             // Add mixmonitor to record the call
             $ext->splice('macro-dial-one', 's', 'dial', new \ext_mixmonitor('','br(/var/run/nethvoice/satellite-r-${SAT_SAFE_UID}.wav)t(/var/run/nethvoice/satellite-t-${SAT_SAFE_UID}.wav)','/var/lib/asterisk/bin/satellite_transcription -u ${SAT_SAFE_UID} -c0 "${SAT_SAFE_DST}" -c1 "${SAT_SAFE_SRC}"'),'', -1);
+            // Sanitize variables to prevent shell injection in mixmonitor command
+            $ext->splice('macro-dial-one', 's', 'dial', new \ext_setvar('SAT_SAFE_SRC', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(cnam)})}'), '', -1);
+            $ext->splice('macro-dial-one', 's', 'dial', new \ext_setvar('SAT_SAFE_DST', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(dst_cnam)})}'), '', -1);
+            $ext->splice('macro-dial-one', 's', 'dial', new \ext_setvar('SAT_SAFE_UID', '${FILTER(A-Za-z0-9._-,${UNIQUEID})}'), '', -1);
             // Add call to Satellite macro in macro-dialout-trunk if there is at least one route with at least one trunk
             $routes = core_routing_list();
             if (!empty($routes)) {
@@ -27,12 +27,12 @@ function satellite_get_config_late($engine) {
                     $routetrunks = core_routing_getroutetrunksbyid($route['route_id']);
                     if (!empty($routetrunks)) {
                         $ext->splice('macro-dialout-trunk', 's', '', new \ext_setvar('DIAL_TRUNK_OPTIONS', '${DIAL_TRUNK_OPTIONS}U(satellite^s^1)'),'', 28);
-                        // Sanitize variables to prevent shell injection in mixmonitor command
-                        $ext->splice('macro-dialout-trunk', 's', '', new \ext_setvar('SAT_SAFE_UID', '${FILTER(A-Za-z0-9._-,${UNIQUEID})}'), '', 28);
-                        $ext->splice('macro-dialout-trunk', 's', '', new \ext_setvar('SAT_SAFE_DST', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(dst_cnam)})}'), '', 28);
-                        $ext->splice('macro-dialout-trunk', 's', '', new \ext_setvar('SAT_SAFE_SRC', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(cnam)})}'), '', 28);
                         // Add mixmonitor to record the call
                         $ext->splice('macro-dialout-trunk', 's', '', new \ext_mixmonitor('','br(/var/run/nethvoice/satellite-r-${SAT_SAFE_UID}.wav)t(/var/run/nethvoice/satellite-t-${SAT_SAFE_UID}.wav)','/var/lib/asterisk/bin/satellite_transcription -u ${SAT_SAFE_UID} -c0 "${SAT_SAFE_DST}" -c1 "${SAT_SAFE_SRC}"'),'', 28);
+                        // Sanitize variables to prevent shell injection in mixmonitor command
+                        $ext->splice('macro-dialout-trunk', 's', '', new \ext_setvar('SAT_SAFE_SRC', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(cnam)})}'), '', 28);
+                        $ext->splice('macro-dialout-trunk', 's', '', new \ext_setvar('SAT_SAFE_DST', '${FILTER(A-Za-z0-9 ._\-\(\),${CDR(dst_cnam)})}'), '', 28);
+                        $ext->splice('macro-dialout-trunk', 's', '', new \ext_setvar('SAT_SAFE_UID', '${FILTER(A-Za-z0-9._-,${UNIQUEID})}'), '', 28);
                         break;
                     }
                 }
