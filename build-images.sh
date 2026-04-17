27c22d04134e2f0c6753747c048397789618802b
Tommaso Ascani <tommaso.ascani@gmail.com>
2026-04-17 12:47:34 +0200

    fix: update container images to use 'fix_transcriptions' tags

--- build-images.sh
+++ build-images.sh
@@ -193,7 +193,7 @@ fi
 reponame="nethvoice-cti-middleware"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:v0.5.2)
+    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:fix_transcriptions)
 
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
@@ -213,7 +213,6 @@ reponame="nethvoice-cti-ui"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
     container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.17)
-
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
     buildah commit "${container}" "${repobase}/${reponame}:${IMAGETAG:-latest}"
@@ -315,7 +314,7 @@ fi
 reponame="nethvoice-satellite"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/satellite:0.1.0)
+    container=$(buildah from ghcr.io/nethesis/satellite:fix_transcriptions)
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
     buildah commit "${container}" "${repobase}/${reponame}:${IMAGETAG:-latest}"
