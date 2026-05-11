1b778423a75991b205673cabd0b9912bccae07be
Antonio <36625268+tonyco97@users.noreply.github.com>
2026-05-11 10:21:40 +0200

    chore(deps): update ghcr.io/nethesis/nethvoice-cti docker tag to v0.15.15 (#807)

--- build-images.sh
+++ build-images.sh
@@ -105,7 +105,7 @@ images+=("${repobase}/${reponame}")
 ##      NethCTI Client     ##
 #############################
 reponame="nethvoice-cti-ui"
-container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.14)
+container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.15)
 
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
