d601f5e1762896447aa4c61f59b204073388508c
Tommaso Ascani <tommaso.ascani@gmail.com>
2026-05-12 16:03:53 +0200

    fix(ports): update Asterisk SIP UDP port handling and related environment variables

--- mariadb/docker-entrypoint-initdb.d/70_asterisk_ports.sh
+++ mariadb/docker-entrypoint-initdb.d/70_asterisk_ports.sh
@@ -5,7 +5,7 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
 
-sip_sql='INSERT INTO `kvstore_Sipsettings` (`key`,`val`,`type`,`id`) VALUES ("udpport-0.0.0.0","'${ASTERISK_SIP_PORT}'",NULL,"noid"), ("tlsport-0.0.0.0","'${ASTERISK_SIPS_PORT}'",NULL,"noid"), ("tcpport-0.0.0.0","'${ASTERISK_SIP_PORT}'",NULL,"noid");'
+sip_sql='INSERT INTO `kvstore_Sipsettings` (`key`,`val`,`type`,`id`) VALUES ("udpport-0.0.0.0","'${ASTERISK_SIP_UDP_PORT}'",NULL,"noid"), ("tlsport-0.0.0.0","'${ASTERISK_SIPS_PORT}'",NULL,"noid"), ("tcpport-0.0.0.0","'${ASTERISK_SIP_PORT}'",NULL,"noid");'
 
 astmanager_sql='INSERT INTO `freepbx_settings` (`keyword`,`value`,`name`,`level`,`description`,`type`,`options`,`defaultval`,`readonly`,`hidden`,`category`,`module`,`emptyok`,`sortorder`) VALUES ("ASTMANAGERPORT","'${ASTMANAGERPORT}'","Asterisk Manager Port",2,"Port for the Asterisk Manager","int","1024,65535","'${ASTMANAGERPORT}'",1,0,"Asterisk Manager","",0,0);'
 
