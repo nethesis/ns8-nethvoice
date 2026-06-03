f1ed9b4f0cd2ae1cbb444f6f7d343546152af163
Antonio <tonyco97.tc@gmail.com>
2026-06-03 15:16:39 +0200

    chore(deps): update ghcr.io/nethesis/nethvoice-cti docker tag to v0.15.17

--- build-images.sh
+++ build-images.sh
@@ -212,7 +212,7 @@ fi
 reponame="nethvoice-cti-ui"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/nethvoice-cti:rebranding)
+    container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.17)
 
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
