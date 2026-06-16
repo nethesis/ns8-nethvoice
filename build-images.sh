912db0067441836f9ab287e69228fabbe8ac7605
Tommaso Ascani <tommaso.ascani@gmail.com>
2026-06-16 09:09:04 +0200

    chore(build): pin middleware image to issue_8039 branch for QA
    Temporary: revert to a released v0.5.x tag before merge.
    
    Refs: NethServer/dev#8039

--- build-images.sh
+++ build-images.sh
@@ -193,7 +193,7 @@ fi
 reponame="nethvoice-cti-middleware"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:v0.5.3)
+    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:issue_8039)
 
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
