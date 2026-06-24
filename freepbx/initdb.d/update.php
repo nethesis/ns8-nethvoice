7340e5d73a2a0e224c5baae9c39d356da6d69d7e
Stefano Fancello <gentoo.stefano@gmail.com>
2026-06-24 11:22:07 +0200

    fix(freepbx): Fix update queries

--- freepbx/initdb.d/update.php
+++ freepbx/initdb.d/update.php
@@ -80,24 +80,33 @@ $stmt->execute();
 // Prefer header matching before IP so proxied static PJSIP trunks are identified
 // from X-Forwarded-* headers.
 $pjsip_identifiers_order = json_encode(['header', 'ip', 'username', 'auth_username', 'anonymous']);
-$stmt = $db->prepare("UPDATE `asterisk`.`kvstore_Sipsettings` SET `val` = ? WHERE `key` = 'pjsip_identifers_order' AND `data` = 'ip,username,anonymous,auth_username'");
+$stmt = $db->prepare("UPDATE `asterisk`.`kvstore_Sipsettings` SET `val` = ? WHERE `key` = 'pjsip_identifers_order' AND `val` = 'ip,username,anonymous,auth_username'");
 $stmt->execute([$pjsip_identifiers_order]);
 
 // Update outbound routes notification_on field
 // If is set to "call" and there is an email address configured for that route,
 // set it to "pattern", if no email is configured set it to empty.
 // "call" option doesn't even works and breaks satellite real time transcription #7795
-$sql = "UPDATE `asterisk`.`outbound_routes` 
-		SET `notification_on` = 'pattern' 
-		WHERE `notification_on` = 'call' AND `route_id` IN (
-			SELECT `route_id` 
-			FROM `asterisk`.`outbound_route_email` 
-			WHERE `emailto` LIKE '%@%');
-		UPDATE `asterisk`.`outbound_routes` 
-		SET `notification_on` = ''
-		WHERE `route_id` NOT IN (
-			SELECT `route_id` 
-			FROM `asterisk`.`outbound_route_email` 
-			WHERE `emailto` LIKE '%@%')";
+$sql = "SELECT COUNT(*) AS `required_tables`
+	FROM `information_schema`.`TABLES`
+	WHERE `TABLE_SCHEMA` = 'asterisk'
+	AND `TABLE_NAME` IN ('outbound_routes', 'outbound_route_email')";
 $stmt = $db->prepare($sql);
 $stmt->execute();
+$res = $stmt->fetch(\PDO::FETCH_ASSOC);
+if ((int) ($res['required_tables'] ?? 0) === 2) {
+	$sql = "UPDATE `asterisk`.`outbound_routes` 
+			SET `notification_on` = 'pattern' 
+			WHERE `notification_on` = 'call' AND `route_id` IN (
+				SELECT `route_id` 
+				FROM `asterisk`.`outbound_route_email` 
+				WHERE `emailto` LIKE '%@%');
+			UPDATE `asterisk`.`outbound_routes` 
+			SET `notification_on` = ''
+			WHERE `route_id` NOT IN (
+				SELECT `route_id` 
+				FROM `asterisk`.`outbound_route_email` 
+				WHERE `emailto` LIKE '%@%')";
+	$stmt = $db->prepare($sql);
+	$stmt->execute();
+}
