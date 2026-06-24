3707ca312462ed02fb6c333d6d7e4f29c362d0aa
Tommaso Ascani <tommaso.ascani@gmail.com>
2026-06-24 15:02:44 +0200

    chore(build): QA testing image for /history/statuses rename (issue_8049) (#868)

--- build-images.sh
+++ build-images.sh
@@ -193,7 +193,7 @@ fi
 reponame="nethvoice-cti-middleware"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:v0.5.6)
+    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:v0.5.7)
 
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
@@ -212,7 +212,7 @@ fi
 reponame="nethvoice-cti-ui"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.19)
+    container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.20)
 
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
