612600a5072b807390163beb8668623026c028ed
Antonio <36625268+tonyco97@users.noreply.github.com>
2026-06-25 16:03:05 +0200

    More sharing options for contacts (#833)

--- build-images.sh
+++ build-images.sh
@@ -193,7 +193,7 @@ fi
 reponame="nethvoice-cti-middleware"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:v0.5.7)
+    container=$(buildah from ghcr.io/nethesis/nethcti-middleware:v0.5.8)
 
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
@@ -212,7 +212,7 @@ fi
 reponame="nethvoice-cti-ui"
 if should_build "${reponame}"; then
     start_timing "${reponame}"
-    container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.20)
+    container=$(buildah from ghcr.io/nethesis/nethvoice-cti:v0.15.21)
 
     # Commit the image
     buildah commit "${container}" "${repobase}/${reponame}"
