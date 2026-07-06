f57694e9b0d4e2f6098e2f88d3327bca1f8a581f
Antonio <tonyco97.tc@gmail.com>
2026-07-06 10:49:39 +0200

    feat(phonebook): add extended contact fields migration for existing installs

--- mariadb/docker-entrypoint-initdb.d/30_nethcti3.cti_phonebook-schema.sql
+++ mariadb/docker-entrypoint-initdb.d/30_nethcti3.cti_phonebook-schema.sql
@@ -40,6 +40,8 @@ CREATE TABLE `cti_phonebook` (
   `linkedin` varchar(255) DEFAULT NULL,
   `workphone2` varchar(25) DEFAULT NULL,
   `cellphone2` varchar(25) DEFAULT NULL,
+  `otherphone` varchar(25) DEFAULT NULL,
+  `otheremail` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `owner_idx` (`owner_id`),
   KEY `wemail_idx` (`workemail`),
@@ -53,5 +55,7 @@ CREATE TABLE `cti_phonebook` (
   KEY `company_idx` (`company`),
   KEY `lastname_idx` (`lastname`),
   KEY `wphone2_idx` (`workphone2`),
-  KEY `cphone2_idx` (`cellphone2`)
+  KEY `cphone2_idx` (`cellphone2`),
+  KEY `ophone_idx` (`otherphone`),
+  KEY `oemail_idx` (`otheremail`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
